60e0da3d6ccab322fe7098dd8dd7592676eaf6fe
mrloiho <loihs@tcbs.com.vn>
2021-11-17 11:04:58 +0700

    test crawl with depth = 10, no limit time = 1h, records users = 23k, records relationships = 105k

--- get_my_follow.py
+++ get_my_follow.py
@@ -12,7 +12,7 @@ dev = 0
 user_ids_scanned = []
 result = []
 need_scan_ids = []
-depth = 6
+depth = 10
 
 follower_file_name = 'follow_relationship.csv'
 follow_relationship_column = ['index', 'user_id', 'follower_id']
@@ -67,6 +67,11 @@ def crawl_data():
     while depth_index <= depth and len(need_scan_ids) > 0:
         scan_followers_by_user_id(user_ids_scanned, need_scan_ids, depth_index, result)
         depth_index += 1
+        print()
+        print()
+        print("<<<===================== Scanned with depth = {} success ==========================>>>".format(depth_index))
+        print()
+        print()
     print("===================== scanned {} actors ==========================".format(len(user_ids_scanned)))
     util.log_method_complete("crawl_data", "", 1)
 
@@ -82,13 +87,15 @@ def write_data():
     _writer_user_f.writerow(user_info_column)
 
     _index = 1
+    _index_user_info = 1
     _user_info_detail_wrote = []
     for _user_info in result:
         _user_info.__class__ = user_info.userInfo
         write_follow_relationship_info(_index, _writer_follow_f, _user_info, _user_info.source_user_id)
         if _user_info.user_id not in _user_info_detail_wrote:
-            write_user_info(_index, _writer_user_f, _user_info)
+            write_user_info(_index_user_info, _writer_user_f, _user_info)
             _user_info_detail_wrote.append(_user_info.user_id)
+            _index_user_info += 1
         _index += 1
     _follow_f.close()
     _user_f.close()
